5f59c8cfd78432775c7e62d10827293f68393223
Artem Bolotov <teeemon@gmail.com>
2023-02-20 15:32:51 +0300

    Initial Commit

---
+++ SuffixArray/SuffixArrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuffixArrayApp.swift
+//  SuffixArray
+//
+//  Created by artembolotov on 20.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SuffixArrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
